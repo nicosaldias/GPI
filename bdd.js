8605ffc27aa8f1357e0c761e3afc1b9bd9c92b0d
nicosaldias <nicosaldias157@gmail.com>
2020-12-29 03:33:09 -0300

    final

--- bdd.js
+++ bdd.js
@@ -1,23 +1,16 @@
-const {Client } = require('pg')
+const {Pool,Client} = require('pg');
+
+
 
 const client = new Client({
   name: 'gestion',
-  user: 'phpmyadmin',
-  password: 'Nico1998!',
-  host: '192.168.18.18',
-  port: 3306
+  user: 'agpena2016',
+  password: 'trafalgar5',
+  host: 'plop.inf.udec.cl',
+  port: 5432
 })
 
-
-client.connect(function(error)  {
-  if(error){
-      message="no me pude conectar"+error;
-      console.error(message);
-      client.end();
-  }
-  console.log("Conectado!");
-});
-
+client.connect();
 module.exports = client;
 
 /*name: 'gestion',
